1187c037550d8275bf3ece226288748663c522fd
ilia makarov <ilia.makarov@me.com>
2025-06-12 15:36:14 +0300

    fix warning with memoization selectors

--- web-client/src/features/cards/cards.selectors.ts
+++ web-client/src/features/cards/cards.selectors.ts
@@ -1,17 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { type RootState } from '@/store';
 import { type CardStatus } from './types';
 
 export const selectCardsState = (state: RootState) => state.cards;
 
-export const selectAllCards = (state: RootState) => {
-  const { entities, ids } = selectCardsState(state);
-  return ids.map((id) => entities[id]);
-};
+export const selectAllCards = createSelector(
+  [selectCardsState],
+  (cardsState) => cardsState.ids.map((id) => cardsState.entities[id]),
+);
 
-export const selectCardsByStatus = (status: CardStatus) => (state: RootState) => {
-  const cards = selectAllCards(state);
-  return cards.filter((card) => card.status === status);
-};
+export const selectCardsByStatus = (status: CardStatus) => createSelector(
+  [selectAllCards],
+  (cards) => cards.filter((card) => card.status === status)
+);
 
 export const selectCardById = (id: string) => (state: RootState) =>
   selectCardsState(state).entities[id];
